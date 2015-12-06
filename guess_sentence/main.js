b0fce9869a4103d2fd3118768811285ba54694da
mkuzdowicz <mkuzdowic@power.com.pl>
2015-12-06 23:46:35 +0100

    add base game logic

--- guess_sentence/main.js
+++ guess_sentence/main.js
@@ -1,18 +1,80 @@
 
-// MAIN ----------------------------------------
+// GLOBAL VARS ------------------------------------
 
 var lettersUl = document.getElementById('letters');
+
+
+var sentenceUl = document.getElementById('sentence');
 
 var LETTERS_ARR = ['a','ą','b','c','ć','d','e','ę','f','g','h','i','j','k','l','ł','m','n','ń','o','ó','p','q','r','s','ś','t','u','v','w','x','y','z','ź','ż'];
 
 var startGameBtn = document.getElementById('startGameBtn');
 
-init();
+var sentences = ['Fantomas','Super Szamson','Hasło'];
 
+var howManyCanYouTry = 5;
+
+var numberOfGuessedLetters = 0;
+
+var actualSentence;
+
+
+// MAIN ----------------------------------------
+
+
+init();
+setActualPassword();
 
 
 
 // FUNCTIONS ------------------------------------------
+
+function checkIfGameOVer() {
+	if(howManyCanYouTry == 0) {
+
+		alert('game over! You lose');
+
+
+	}
+
+	if(numberOfGuessedLetters == actualSentence.length) {
+
+		alert('game over! You winn');
+
+	}
+}
+
+
+function setActualPassword() {
+
+	var randomNum = Math.floor(Math.random() * 3);
+
+	actualSentence = sentences[randomNum].toUpperCase();
+
+	for(var i = 0; i < actualSentence.length; i++) {
+
+		var liNode = document.createElement("LI"); 
+
+		liNode.setAttribute('id', 'sentenceIndex'+i);
+
+		 sentenceUl.appendChild(liNode);
+		
+	}
+
+
+
+}
+
+function resetDomElementsAndVars() {
+
+	sentenceUl.innerHTML = '';
+	lettersUl.innerHTML = '';
+	howManyCanYouTry = 5;
+
+    numberOfGuessedLetters = 0;
+
+}
+
 
 function init(){
 
@@ -20,17 +82,69 @@ function init(){
 
 		var liNode = document.createElement("LI"); 
 
+		 liNode.setAttribute('id', LETTERS_ARR[i]);
 		 liNode.innerHTML = LETTERS_ARR[i];
+
+		 	liNode.addEventListener("click", function(e){
+
+				 	var letter = (e.target.id).toUpperCase();
+				 	checkPlayerMove(letter);
+
+		 		});
+
 		 lettersUl.appendChild(liNode);
 
 	}
 
+	document.getElementById('howMany').innerHTML = howManyCanYouTry;
+
 }
+
+
+function checkPlayerMove(letter) {
+
+	if(actualSentence.indexOf(letter) > -1) {
+
+
+		setGuesseddLetters(letter);
+
+	} else {
+
+		 howManyCanYouTry--;
+		 document.getElementById('howMany').innerHTML = howManyCanYouTry;
+	}
+
+	checkIfGameOVer();
+
+}
+
+function setGuesseddLetters (letter) {
+
+
+	for(var i = 0; i < actualSentence.length; i++) {
+
+
+			if(actualSentence[i] == letter){
+
+				document.getElementById('sentenceIndex'+i).innerHTML = letter;
+				numberOfGuessedLetters++;
+			}
+
+	}
+
+
+}
+
+
+// EVENTS LISTENERS
+
 
 
 startGameBtn.addEventListener("click", function(e){
 
-	alert(e.target.id);
+	resetDomElementsAndVars();
+	init();
+	setActualPassword();
 
 });
 
